21e0c670784d41ae8559beb7abef94a037e4cd65
Londomloto <roso.sasongko@gmail.com>
2016-03-11 04:07:08 +0700

    Create TreeModel.php

---
+++ src/Londo/TreeModel.php
@@ -0,0 +1,68 @@
+<?php
+namespace Londo;
+
+use Phalcon\Mvc\Model;
+
+class TreeModel extends Model {
+    
+    public function parent() {
+    
+    }
+    
+    public function prev() {
+    
+    }
+    
+    public function next() {
+    
+    }
+    
+    public function ancestors() {
+    
+    }
+    
+    public function descendants() {
+    
+    }
+    
+    public function children() {
+    
+    }
+    
+    public function siblings() {
+    
+    }
+    
+    public function cascade($callback) {
+    
+    }
+    
+    public function bubble($callback) {
+        
+    }
+    
+    public function append(Model $node) {
+    
+    }
+    
+    public function prepend(Model $node) {
+    
+    }
+    
+    public function before(Model $node) {
+    
+    }
+    
+    public function after(Model $node) {
+    
+    }
+    
+    public function insertBefore(Model $node) {
+    
+    }
+    
+    public function insertAfter(Model $node) {
+    
+    }
+    
+}
